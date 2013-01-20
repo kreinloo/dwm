993b43b0700ea21a0cc8d2e7e977a44372781f41
Kristjan Reinloo <kristjanreinloo@gmail.com>
2013-01-20 22:41:34 +0200

    Some mistakes fixed.

--- config.h
+++ config.h
@@ -36,6 +36,7 @@ static const Layout layouts[] = {
 
 /* key definitions */
 #define MODKEY Mod1Mask
+#define MOD4   Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
